b30a3234342aa87e7024cbfc8d425d545e156254
Alexander Glushchenko <a.glushchenko@cmachine.ru>
2022-08-08 23:22:23 +1000

    Added Redis

--- RedisAPI/Program.cs
+++ RedisAPI/Program.cs
@@ -1,6 +1,12 @@
+using StackExchange.Redis;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddSingleton<IConnectionMultiplexer>(opt =>
+    ConnectionMultiplexer.Connect(builder.Configuration.GetConnectionString("RedisConnection"))
+);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
